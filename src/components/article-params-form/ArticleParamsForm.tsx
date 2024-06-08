ec7f22c7c47f27fbf7ed35208a78e21c051677f9
Daniil Glushakov <anydnny@gmail.com>
2024-06-08 16:12:04 +0300

    Добавлен основной функционал

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,16 +1,136 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { Text } from '../text';
+import { Select } from '../select';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
 
+import {
+	ArticleStateType,
+	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+} from 'src/constants/articleProps';
+
+import { FormEvent, SetStateAction, useState, useEffect, useRef } from 'react';
+import clsx from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type Test = {
+	submitForm: (e: FormEvent) => void;
+	resetForm: () => void;
+	formState: ArticleStateType;
+	appState: ArticleStateType;
+	setFormState: React.Dispatch<SetStateAction<ArticleStateType>>;
+};
+export const ArticleParamsForm = (props: Test) => {
+	const [open, setOpen] = useState(false);
+	const formRef = useRef<HTMLFormElement | null>(null);
+
+	useEffect(() => {
+		if (!open) return;
+
+		function handleOutsideClick(e: MouseEvent) {
+			if (
+				e.target instanceof Node &&
+				formRef.current &&
+				!formRef.current.contains(e.target)
+			) {
+				setOpen(false);
+			}
+		}
+
+		function handleEscClick(e: KeyboardEvent) {
+			if (e.key === 'Escape') {
+				setOpen(false);
+			}
+		}
+
+		document.addEventListener('keydown', handleEscClick);
+		document.addEventListener('mousedown', handleOutsideClick);
+
+		return () => {
+			document.removeEventListener('keydown', handleEscClick);
+			document.removeEventListener('mousedown', handleOutsideClick);
+		};
+	}, [open, formRef]);
+
+	const formStyles = clsx({
+		[styles.container]: true,
+		[styles.container_open]: open,
+	});
+
+	function handleSubmitForm(e: FormEvent) {
+		props.submitForm(e);
+		setOpen(false);
+	}
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton handle={() => setOpen((prev) => !prev)} open={open} />
+			<aside className={formStyles}>
+				<form
+					className={styles.form}
+					onSubmit={(e) => handleSubmitForm(e)}
+					ref={formRef}>
+					<Text as='h1' size={31} weight={800} uppercase align='left'>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={props.formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						title='Шрифт'
+						onChange={(selected) =>
+							props.setFormState({
+								...props.formState,
+								fontFamilyOption: selected,
+							})
+						}
+					/>
+					<RadioGroup
+						title='Размер'
+						options={fontSizeOptions}
+						selected={props.formState.fontSizeOption}
+						name='fontsize'
+						onChange={(selected) =>
+							props.setFormState({
+								...props.formState,
+								fontSizeOption: selected,
+							})
+						}
+					/>
+					<Select
+						title='Цвет шрифта'
+						options={fontColors}
+						selected={props.formState.fontColor}
+						onChange={(selected) =>
+							props.setFormState({ ...props.formState, fontColor: selected })
+						}
+					/>
+					<Separator />
+					<Select
+						options={backgroundColors}
+						selected={props.formState.backgroundColor}
+						title='Цвет фона'
+						onChange={(selected) =>
+							props.setFormState({
+								...props.formState,
+								backgroundColor: selected,
+							})
+						}
+					/>
+					<Select
+						options={contentWidthArr}
+						selected={props.formState.contentWidth}
+						title='Ширина контента'
+						onChange={(selected) =>
+							props.setFormState({ ...props.formState, contentWidth: selected })
+						}
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
+						<Button title='Сбросить' type='reset' onClick={props.resetForm} />
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
